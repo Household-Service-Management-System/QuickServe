28305dc7833ba9c73bd9e0c31d83244470045ce2
surajshinde2340 <surajshinde2340@gmail.com>
2026-01-25 12:14:41 +0530

    DashBoard Infomation

---
+++ spring_boot_backend/src/main/java/com/backend/controller/AdminController.java
@@ -0,0 +1,47 @@
+package com.backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.backend.service.AdminService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/admin")
+@RequiredArgsConstructor
+public class AdminController {
+
+    private final AdminService adminService;
+
+    @GetMapping(
+    	    value = "/dashboard",
+    	    produces = "application/json"
+    	)
+    public ResponseEntity<?> dashboard() {
+        return ResponseEntity.ok(adminService.adminInfo());
+        
+    }
+
+    @GetMapping("/customerComplaints")
+    public ResponseEntity<?> getCustomerComplaints() {
+        return ResponseEntity.ok("suraj");
+    }
+
+    @GetMapping("/serviceProviderComplaints")
+    public ResponseEntity<?> serviceProviderComplaints() {
+        return ResponseEntity.ok("suraj");
+    }
+
+    @GetMapping("/pendingRequests")
+    public ResponseEntity<?> pendingRequests() {
+        return ResponseEntity.ok("suraj");
+    }
+
+    @GetMapping("/paymentRecords")
+    public ResponseEntity<?> paymentRecords() {
+        return ResponseEntity.ok("suraj");
+    }
+}
