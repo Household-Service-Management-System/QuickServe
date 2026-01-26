171de474c20ed84fa538d86226bfb7f3b61f4fe6
surajshinde2340 <surajshinde2340@gmail.com>
2026-01-26 23:30:30 +0530

    add getPaymentList getServiceProviderDetails getAllServiceProvidersDetails

--- spring_boot_backend/src/main/java/com/backend/service/AdminService.java
+++ spring_boot_backend/src/main/java/com/backend/service/AdminService.java
@@ -7,9 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.backend.dto.AdminDashBoardInfo;
-
+import com.backend.dto.DisputeComplaintDTO;
 import com.backend.dto.DisputeDetailsDTO;
+import com.backend.dto.PaymentBookingUserDTO;
+import com.backend.dto.ServiceProviderDetailsDTO;
 import com.backend.dto.ServiceProviderResponseDTO;
+import com.backend.entities.PaymentStatus;
 import com.backend.entities.ServiceProvider;
 import com.backend.repository.DisputeRepository;
 import com.backend.repository.PaymentRepository;
@@ -40,9 +43,13 @@ public class AdminService {
 		    long pendingRequests =
 		            serviceProviderRepository.countByVerificationStatusFalse();
 
+		    
+		    double totalSuccessAmount =
+		            paymentRepository.getTotalAmountByStatus(PaymentStatus.SUCCESS);
+		    
 		    return new AdminDashBoardInfo(
 		            totalServiceProviders,
-		            0L,
+		            totalSuccessAmount,
 		            totalCustomers,
 		            pendingRequests
 		    );
@@ -58,6 +65,21 @@ public class AdminService {
 		   
 		    return serviceProviderRepository.findVerifiedProviders();
 		}
+	 
+	 public List<PaymentBookingUserDTO> getPaymentList() { 
+		return paymentRepository.fetchPaymentBookingUserDetails();
+	 }
+	 
+	 public ServiceProviderDetailsDTO getServiceProviderDetails(Long userId) {
+		    return serviceProviderRepository
+		            .fetchServiceProviderDetailsByUserId(userId);
+		}
+	 
+	 public List<ServiceProvider> getAllServiceProvidersDetails() {
+	        return serviceProviderRepository.findAll();
+	    }
 
+	 
+	
 
 }
