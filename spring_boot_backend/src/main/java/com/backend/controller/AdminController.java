cd0a3fbfc5525f47a7b18d5ac6951432e04d19b6
surajshinde2340 <surajshinde2340@gmail.com>
2026-01-26 23:15:36 +0530

    add serviceProviderDetail , getAllServiceProviders

--- spring_boot_backend/src/main/java/com/backend/controller/AdminController.java
+++ spring_boot_backend/src/main/java/com/backend/controller/AdminController.java
@@ -1,10 +1,17 @@
 package com.backend.controller;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.backend.dto.ServiceProviderDetailsDTO;
+import com.backend.dto.ServiceProviderResponseDTO;
+import com.backend.entities.ServiceProvider;
 import com.backend.service.AdminService;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/admin")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173/")
 public class AdminController {
 
     private final AdminService adminService;
@@ -42,6 +50,23 @@ public class AdminController {
 
     @GetMapping("/paymentRecords")
     public ResponseEntity<?> paymentRecords() {
-        return ResponseEntity.ok("suraj");
+        return ResponseEntity.ok( adminService.getPaymentList());
     }
+    
+    @GetMapping("/serviceProviderDetail/{userId}")
+    public ResponseEntity<ServiceProviderDetailsDTO> serviceProviderDetail(
+            @PathVariable Long userId
+    ) {
+    	
+        return ResponseEntity.ok(
+        		adminService.getServiceProviderDetails(userId)
+        );
+    }
+    
+    @GetMapping("/service-providers")
+    public ResponseEntity<List<ServiceProviderResponseDTO>> getAllServiceProviders() {
+        return ResponseEntity.ok(adminService.getAllServiceProviders());
+    }
+
+
 }
