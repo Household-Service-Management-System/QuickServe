1224cddd311c83fcd8f470c4da7d8db974083459
surajshinde2340 <surajshinde2340@gmail.com>
2026-01-28 19:09:38 +0530

    add method in user respo

--- spring_boot_backend/src/main/java/com/backend/repository/UserRepository.java
+++ spring_boot_backend/src/main/java/com/backend/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.backend.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,6 +9,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.backend.dtos.CustomerDTO;
+import com.backend.entities.Role;
 import com.backend.entities.User;
 
 @Repository
@@ -19,5 +21,12 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	
 	@Query("SELECT u FROM User u WHERE u.id = :id")
 	Optional<User> findUserByIdCustom(@Param("id") Long id);
+	
+	@Query("SELECT u FROM User u WHERE u.role = ROLE_USER")
+	List<User> findUsersByRole();
+	
+	
+
+
 }
 
