08c7fcb65d91acfd2af940ee02f30262e6a4c7bb
surajshinde2340 <surajshinde2340@gmail.com>
2026-01-28 19:01:10 +0530

    add jpql

--- spring_boot_backend/src/main/java/com/backend/repository/DisputeRepository.java
+++ spring_boot_backend/src/main/java/com/backend/repository/DisputeRepository.java
@@ -1,9 +1,11 @@
 package com.backend.repository;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.backend.dto.DisputeComplaintDTO;
@@ -20,14 +22,30 @@ public interface DisputeRepository extends JpaRepository<Dispute, Long> {
 		        u.email,
 		        u.phone,
 		        d.description,
-		        d.status
+		        d.status,
+		        d.id
+
 		    )
 		    FROM Dispute d
 		    JOIN d.raisedBy u
 		""")
 		List<DisputeDetailsDTO> fetchDisputeDetails();
-	
-	
+	@Query("""
+		    SELECT d
+		    FROM Dispute d
+		    JOIN FETCH d.booking b
+		    JOIN FETCH b.serviceProvider sp
+		    JOIN FETCH sp.user
+		    JOIN FETCH d.raisedBy
+		    LEFT JOIN FETCH d.resolvedBy
+		    LEFT JOIN FETCH sp.services
+		    WHERE d.id = :disputeId
+		""")
+		Optional<Dispute> findDisputeWithAllJoins(
+		        @Param("disputeId") Long disputeId
+		);
+
+
 
 }
 
